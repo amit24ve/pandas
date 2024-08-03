8d671ffc765a923ea383c5a5c1bfa9f2143c36f5
Amit Kumar <55157688+amit24ve@users.noreply.github.com>
2024-08-03 23:44:57 +0530

    Update day1.py

--- day1.py
+++ day1.py
@@ -156,3 +156,9 @@ print(df.loc[['a','b']])# import pandas as pa
 
 # a="amit".split()
 # print(a)
+
+import pandas as pd
+
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    students.rename(columns={"id":"student_id","first":"first_name","last":"last_name","age":"age_in_years"},inplace=True)
+    return students
